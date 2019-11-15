5a3cf3584771abfc2eb32f914a8804061a6c3f6b
pacoyeung <paco.ambilab@icloud.com>
2019-11-15 16:36:16 +0800

    init

--- Pods/Target Support Files/Pods-ambi_ios_firebase_analytic/Pods-ambi_ios_firebase_analytic-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ambi_ios_firebase_analytic : NSObject
-@end
-@implementation PodsDummy_Pods_ambi_ios_firebase_analytic
-@end
--- Pods/Target Support Files/Pods-ambi_ios_firebase_analyticTests/Pods-ambi_ios_firebase_analyticTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ambi_ios_firebase_analyticTests : NSObject
-@end
-@implementation PodsDummy_Pods_ambi_ios_firebase_analyticTests
-@end
